077d262de7bd0c6d92afa232ad16353414b407b9
Blinnikov Andrei <goofinator@mail.ru>
2020-04-05 21:25:20 +0300

    feat: serve /weather endpoint with some dummy value

--- weatherhttp.go
+++ weatherhttp.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 )
 
 const defaultPort = 8080
@@ -13,7 +15,17 @@ func getPort() (port int) {
 	return port
 }
 
+func weather(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "some weather report")
+}
+
 func main() {
 	port := getPort()
-	fmt.Println(port)
+
+	http.HandleFunc("/weather", weather)
+
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err != nil {
+		log.Fatalf("unexpected error while ListenAndServe: %s", err)
+	}
 }
