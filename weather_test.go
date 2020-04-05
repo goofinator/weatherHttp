af1f2ff84dcbf2a69c86a4eca04d27343f2c1044
Blinnikov Andrei <goofinator@mail.ru>
2020-04-05 22:43:48 +0300

    feat: return constant valid json on /weather endpoint

--- weather_test.go
+++ weather_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -13,17 +14,22 @@ func TestWeatherHandler(t *testing.T) {
 	}
 
 	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(weather)
+	handler := http.HandlerFunc(weatherHandler)
 	handler.ServeHTTP(rr, req)
 
 	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("unexpected status code:\ngot: %v,\nwant: %v",
-			status, http.StatusOK)
+		t.Errorf("unexpected status code:\nwant: %v\ngot: %v",
+			http.StatusOK, status)
 	}
 
-	expected := "some weather report"
-	if rr.Body.String() != expected {
-		t.Errorf("unexpected body: got %v want %v",
-			rr.Body.String(), expected)
+	testWeather(t, rr.Body.String())
+}
+
+func testWeather(t *testing.T, body string) {
+	weatherValue := weather{}
+	err := json.Unmarshal([]byte(body), &weatherValue)
+	if err != nil {
+		t.Errorf("unexpected error on json.Unmarshal:\nwant: %v\ngot: %v",
+			nil, err)
 	}
 }
--- weatherhttp.go
+++ weatherhttp.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
@@ -9,6 +10,11 @@ import (
 
 const defaultPort = 8080
 
+type weather struct {
+	ID      int    `json:"id"`
+	Feeling string `json:"feeling"`
+}
+
 func getPort() (port int) {
 	flag.IntVar(&port, "port", defaultPort, "port to connect this server")
 	flag.Parse()
@@ -16,14 +22,26 @@ func getPort() (port int) {
 }
 
 // Weather handles the request on /weather endpoint
-func weather(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "some weather report")
+func weatherHandler(w http.ResponseWriter, req *http.Request) {
+	weatherValue := weather{
+		ID:      123,
+		Feeling: "shdjkah182y3jasbnd",
+	}
+
+	prettyJSON, err := json.MarshalIndent(weatherValue, "", "    ")
+	if err != nil {
+		msg := fmt.Sprintf("error on json.MarshalIndent: %s.", err)
+		http.Error(w, msg, http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, string(prettyJSON))
 }
 
 func main() {
 	port := getPort()
 
-	http.HandleFunc("/weather", weather)
+	http.HandleFunc("/weather", weatherHandler)
 
 	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 	if err != nil {
